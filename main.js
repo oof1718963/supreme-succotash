15974430a79f258cda4f2e2bc331f4f6ee638462
oof1718963 <75444840+oof1718963@users.noreply.github.com>
2021-11-01 23:37:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+import { fluentTab, fluentTabs, fluentTabPanel, provideFluentDesignSystem } from "https://unpkg.com/@fluentui/web-components@2.0.0";
+
+provideFluentDesignSystem().register(fluentTab(), fluentTabs(), fluentTabPanel());
